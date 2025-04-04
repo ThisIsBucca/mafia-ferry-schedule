73323f1e3451d389e09be47fc386c9c111d63da9
ThisIsBucca <buccaphilox0104@gmail.com>
2025-04-03 23:15:00 -0700

    23commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,17 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/main.jsx')
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
   base: '/',
   resolve: {
