94adfe82dadfd1582e4a1d38045f0cb67bb66df2
ThisIsBucca <buccaphilox0104@gmail.com>
2025-04-03 21:10:35 -0700

    19commit

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: true
   },
-  base: './',
+  base: '/',
   server: {
     headers: {
       'Content-Type': 'application/javascript'
